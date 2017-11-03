b64a270fae48d29140ddb81e93009bb031c4758b
Rb2030 <rossmail2050@gmail.com>
2017-11-03 13:24:21 +0000

    first commit

---
+++ facebook_page/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  facebook_page
+//
+//  Created by Ross on 30/10/2017.
+//  Copyright © 2017 Ross. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ facebook_page/TopBar.swift
@@ -0,0 +1,21 @@
+//
+//  TopBar.swift
+//  facebook_page
+//
+//  Created by Ross on 30/10/2017.
+//  Copyright © 2017 Ross. All rights reserved.
+//
+
+import UIKit
+
+class TopBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
